ec607bf4bacc9e10ef9f8baaff2d5411c50c1463
EclosionASBL <admin@eclosion.be>
2025-06-09 16:42:38 +0200

    Fix missing useNavigate import in RegistrationsPage

--- src/pages/registrations/RegistrationsPage.tsx
+++ src/pages/registrations/RegistrationsPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { supabase } from '../../lib/supabase';
 import { useWaitingListStore } from '../../stores/waitingListStore';
 import { useCartStore } from '../../stores/cartStore';
